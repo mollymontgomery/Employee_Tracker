a48fe300c0bdd358bed2a439de906de9b21af070
Molly Montgomery <Molly@mollys-air-2.lan>
2022-01-02 11:18:27 -0600

    added option lists for user to choose from

--- index.js
+++ index.js
@@ -2,14 +2,54 @@ const mysql = require('mysql12');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const questions = () => {
-    return inquirer.prompt ([
-    {
+function questions() {
+    inquirer.prompt ({
         type: 'list',
         name: 'choice',
         message: 'What would you like to do?',
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Exit"]
-    }
-    ])
+        choices: [
+            "View all departments", 
+            "View all roles",
+            "View all employees", 
+            "Add a department", 
+            "Add a role", 
+            "Add an employee", 
+            "Update employee role",
+            "Delete employee", 
+            "Exit"
+        ]
+    }).then(function (answer){
+        switch (answer.action){
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update employee role':
+                updateRole();
+                break;
+            case 'Delete employee':
+                deleteEmployee();
+                break;
+            case 'EXIT':
+                exitApp();
+                break;
+            default:
+                break;
+        }
+    })
     
 }
